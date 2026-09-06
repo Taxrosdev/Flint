01b0ca4267946bd8f6bf43f8148010b2aa3af130
Taxrosdev <taxrosdev@mailbox.org>
2026-09-06 12:01:04 +1200

    tests: remove weird todo -> panics test?
    These tests should NEVER have existed. What was I thinking?

--- src/chunks/hash.rs
+++ src/chunks/hash.rs
@@ -37,25 +37,9 @@ mod tests {
         let data = b"hello world";
         let hash = hash(HashKind::Blake3, data);
         // Blake3 hash of "hello world"
-        assert_eq!(hash, "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24");
-    }
-
-    #[test]
-    #[should_panic(expected = "not yet implemented")]
-    fn test_hash_sha512_panics() {
-        let _ = hash(HashKind::Sha512, b"test");
-    }
-
-    #[test]
-    #[should_panic(expected = "not yet implemented")]
-    fn test_hash_sha256_panics() {
-        let _ = hash(HashKind::Sha256, b"test");
-    }
-
-    #[test]
-    fn test_hash_kind_display() {
-        assert_eq!(format!("{}", HashKind::Blake3), "Blake3");
-        assert_eq!(format!("{}", HashKind::Sha512), "Sha512");
-        assert_eq!(format!("{}", HashKind::Sha256), "Sha256");
+        assert_eq!(
+            hash,
+            "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24"
+        );
     }
 }
